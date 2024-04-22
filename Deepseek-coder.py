355a1f4f7ca30de2a0a63a0711c6b3037c61f548
Tom Cappendijk <tom.cappendijk@student.uva.nl>
2024-04-22 14:44:25 +0200

    Added instruct version of deepseek

--- Deepseek-coder.py
+++ Deepseek-coder.py
@@ -1,3 +1,5 @@
+# deepseek-coder-6.7b-base
+
 import time
 
 from transformers import AutoTokenizer, AutoModelForCausalLM
@@ -16,3 +18,15 @@ outputs = model.generate(**inputs, max_length=128)
 print(tokenizer.decode(outputs[0], skip_special_tokens=True))
 
 print(f"Total time taken: {time.time() - start_time} seconds")
+
+# deepseek-coder-6.7b-instruct
+
+tokenizer = AutoTokenizer.from_pretrained("deepseek-ai/deepseek-coder-6.7b-instruct", trust_remote_code=True)
+model = AutoModelForCausalLM.from_pretrained("deepseek-ai/deepseek-coder-6.7b-instruct", trust_remote_code=True, torch_dtype=torch.bfloat16).cuda()
+messages=[
+    { 'role': 'user', 'content': "write a quick sort algorithm in python."}
+]
+inputs = tokenizer.apply_chat_template(messages, add_generation_prompt=True, return_tensors="pt").to(model.device)
+# tokenizer.eos_token_id is the id of <|EOT|> token
+outputs = model.generate(inputs, max_new_tokens=512, do_sample=False, top_k=50, top_p=0.95, num_return_sequences=1, eos_token_id=tokenizer.eos_token_id)
+print(tokenizer.decode(outputs[0][len(inputs[0]):], skip_special_tokens=True))
