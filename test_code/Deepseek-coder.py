8185228cedca918d1db815433a181a418f96e967
Tom Cappendijk <tom.cappendijk@student.uva.nl>
2024-04-23 13:59:53 +0200

    Added the pretrained model on the correct gpu

--- test_code/Deepseek-coder.py
+++ test_code/Deepseek-coder.py
@@ -11,7 +11,8 @@ device1 = torch.device("cuda:1")
 start_time = time.time()
 tokenizer = AutoTokenizer.from_pretrained("deepseek-ai/deepseek-coder-6.7b-base", local_files_only=True)
 model = AutoModelForCausalLM.from_pretrained("deepseek-ai/deepseek-coder-6.7b-base", local_files_only=True, torch_dtype=torch.
-bfloat16).cuda()
+bfloat16)
+model = model.to(device0)
 
 print(f"Loading model and tokiner took: {time.time() - start_time} seconds")
 
@@ -25,12 +26,12 @@ print(f"Total time taken: {time.time() - start_time} seconds")
 # deepseek-coder-6.7b-instruct
 
 tokenizer = AutoTokenizer.from_pretrained("deepseek-ai/deepseek-coder-6.7b-instruct", trust_remote_code=True)
-model = AutoModelForCausalLM.from_pretrained("deepseek-ai/deepseek-coder-6.7b-instruct", trust_remote_code=True, torch_dtype=torch.bfloat16).cuda()
+model = AutoModelForCausalLM.from_pretrained("deepseek-ai/deepseek-coder-6.7b-instruct", trust_remote_code=True, torch_dtype=torch.bfloat16)
+model = model.to(device1)
 messages=[
     { 'role': 'user', 'content': "write a quick sort algorithm in python."}
 ]
 
-device = torch.device("cuda:1")
 inputs = tokenizer.apply_chat_template(messages, add_generation_prompt=True, return_tensors="pt").to(device1)
 # tokenizer.eos_token_id is the id of <|EOT|> token
 outputs = model.generate(inputs, max_new_tokens=512, do_sample=False, top_k=50, top_p=0.95, num_return_sequences=1, eos_token_id=tokenizer.eos_token_id)
