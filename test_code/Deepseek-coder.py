d3ecaefbe4e364b8684c49e761340fbd7ce0f907
Tom Cappendijk <tom.cappendijk@student.uva.nl>
2024-04-23 14:07:28 +0200

    Adjusted the prompt

--- test_code/Deepseek-coder.py
+++ test_code/Deepseek-coder.py
@@ -16,7 +16,7 @@ model = model.to(device0)
 
 print(f"Loading model and tokiner took: {time.time() - start_time} seconds")
 
-input_text = "#write a quick sort algorithm"
+input_text = "#write a quick sort algorithm. Only provide the code with no additional text"
 inputs = tokenizer(input_text, return_tensors="pt").to(device0)
 outputs = model.generate(**inputs, max_length=128)
 print(tokenizer.decode(outputs[0], skip_special_tokens=True))
@@ -29,7 +29,7 @@ tokenizer = AutoTokenizer.from_pretrained("deepseek-ai/deepseek-coder-6.7b-instr
 model = AutoModelForCausalLM.from_pretrained("deepseek-ai/deepseek-coder-6.7b-instruct", trust_remote_code=True, torch_dtype=torch.bfloat16)
 model = model.to(device1)
 messages=[
-    { 'role': 'user', 'content': "write a quick sort algorithm in python."}
+    { 'role': 'user', 'content': "write a quick sort algorithm in python. Only provide the code with no additional text"}
 ]
 
 inputs = tokenizer.apply_chat_template(messages, add_generation_prompt=True, return_tensors="pt").to(device1)
