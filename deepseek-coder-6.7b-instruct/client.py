b262433e2cf74579994557131a84c9eef517665c
Tom Cappendijk <tom.cappendijk@student.uva.nl>
2024-04-24 14:42:38 +0200

    Made the client simpeler

--- deepseek-coder-6.7b-instruct/client.py
+++ deepseek-coder-6.7b-instruct/client.py
@@ -1,34 +1,30 @@
 import socket
 import argparse
 
-def client(ip_address, port, prompt: str):
+def client(prompt: str):
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    server_address = (ip_address, port)
+    server_address = ('localhost', 12345)  # localhost and port 12345
     client_socket.connect(server_address)
 
     try:
+        prompt = input("Enter a prompt: ")
         client_socket.sendall(prompt.encode())
 
         data = client_socket.recv(1024)
-        print("Code recieved from server:")
-        print(data.decode(), end="")
+        print("Received from server:", data.decode())
     finally:
         client_socket.close()
 
 def main():
     parser = argparse.ArgumentParser(description="Client for DeepSeekCoder 6.7b Instruct")
-    parser.add_argument("--server", type=str, default="localhost", help="Server IP address")
-    parser.add_argument("--port", type=int, default=12345, help="Port number")
     parser.add_argument("--prompt", type=str, help="Prompt to send to the server")
     args = parser.parse_args()
 
     prompt = args.prompt
-    ip_address = args.server
-    port = args.port
 
     if prompt:
-        client(ip_address, port, prompt)
+        client(prompt)
     else:
         print("Prompt is required. Use --prompt argument to specify the prompt.")
         parser.print_help()
