aa833d69a827fd1ccc7b739f6411f33bc6f5049a
Tom Cappendijk <tom.cappendijk@student.uva.nl>
2024-04-27 21:24:34 +0200

    Adjusted llama code

--- test_code/Llama.py
+++ test_code/Llama.py
@@ -1,5 +1,8 @@
 # Load model directly
 from transformers import AutoTokenizer, AutoModelForCausalLM
+import torch
 
+print("Loading tokenizer...")
 tokenizer = AutoTokenizer.from_pretrained("CodeLlama-13b-Instruct-hf")
-model = AutoModelForCausalLM.from_pretrained("CodeLlama-13b-Instruct-hf")
+print("Loading model...")
+model = AutoModelForCausalLM.from_pretrained("CodeLlama-13b-Instruct-hf", torch_dtype=torch.bfloat16)
