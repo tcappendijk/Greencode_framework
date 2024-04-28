843d0048ffc3aac84b30f139a413de0e6f3e623e
Tom Cappendijk <tom.cappendijk@student.uva.nl>
2024-04-28 16:52:29 +0200

    Adjusted the llama test code

--- test_code/Llama.py
+++ test_code/Llama.py
@@ -5,4 +5,12 @@ import torch
 print("Loading tokenizer...")
 tokenizer = AutoTokenizer.from_pretrained("CodeLlama-7b-Instruct-hf")
 print("Loading model...")
-model = AutoModelForCausalLM.from_pretrained("CodeLlama-7b-Instruct-hf", torch_dtype=torch.bfloat16)
+model = AutoModelForCausalLM.from_pretrained("CodeLlama-7b-Instruct-hf", torch_dtype=torch.bfloat16).cuda()
+
+input_text = "#write a quick sort algorithm"
+inputs = tokenizer(input_text, return_tensors="pt").to(model.device)
+outputs = model.generate(**inputs, max_length=128)
+print(tokenizer.decode(outputs[0], skip_special_tokens=True))
+
+while True:
+    pass
