43ec8935056e3ff1b2d9c167acee0af58bf192ee
Tom Cappendijk <tom.cappendijk@student.uva.nl>
2024-04-28 15:11:54 +0200

    Switched to the 7B param instruct model

--- test_code/Llama.py
+++ test_code/Llama.py
@@ -3,6 +3,6 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 import torch
 
 print("Loading tokenizer...")
-tokenizer = AutoTokenizer.from_pretrained("CodeLlama-13b-Instruct-hf")
+tokenizer = AutoTokenizer.from_pretrained("CodeLlama-7b-Instruct-hf")
 print("Loading model...")
-model = AutoModelForCausalLM.from_pretrained("CodeLlama-13b-Instruct-hf", torch_dtype=torch.bfloat16)
+model = AutoModelForCausalLM.from_pretrained("CodeLlama-7b-Instruct-hf", torch_dtype=torch.bfloat16)
