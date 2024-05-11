e140da4fd85fb5bfc40b878cd7ab88f1f15c418d
Tom Cappendijk <tom.cappendijk@student.uva.nl>
2024-05-11 22:31:29 +0200

    added vllm to deepseek coder

--- test_code/CodeLLama.py
+++ test_code/CodeLLama.py
@@ -4,11 +4,11 @@ from transformers import pipeline
 import os
 
 # Set environment variable to specify CUDA_VISIBLE_DEVICES
-os.environ["CUDA_VISIBLE_DEVICES"] = "0,1"  # Specify the GPUs you want to use
+# os.environ["CUDA_VISIBLE_DEVICES"] = "0,1"  # Specify the GPUs you want to use
 
 custom_cache_dir = "/data/volume_2"
 token = "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"
-model_name = "meta-llama/CodeLlama-70b-Instruct-hf"
+model_name = "meta-llama/CodeLlama-7b-Instruct-hf"
 
 tokenizer = AutoTokenizer.from_pretrained(model_name, cache_dir=custom_cache_dir, token=token)
 
@@ -16,13 +16,13 @@ tokenizer = AutoTokenizer.from_pretrained(model_name, cache_dir=custom_cache_dir
 model = AutoModelForCausalLM.from_pretrained(model_name, cache_dir=custom_cache_dir, token=token, torch_dtype=torch.bfloat16)
 model.to("cuda")
 
-# Create a pipeline
-code_generator = pipeline('text-generation', model=model, tokenizer=tokenizer, num_workers=8, device=0, framework='pt', max_length=1000, pad_token_id=tokenizer.eos_token_id)
+# # Create a pipeline
+# code_generator = pipeline('text-generation', model=model, tokenizer=tokenizer, num_workers=8, device=0, framework='pt', max_length=1000, pad_token_id=tokenizer.eos_token_id)
 
-# Generate code for an input string
-input_string = "Write a python function to calculate the factorial of a number"
-generated_code = code_generator(input_string, max_length=100)[0]['generated_text']
-print(generated_code)
+# # Generate code for an input string
+# input_string = "Write a python function to calculate the factorial of a number"
+# generated_code = code_generator(input_string, max_length=100)[0]['generated_text']
+# print(generated_code)
 
 
 # device_ids = [0, 1, 2, 3, 4, 5, 6, 7]
