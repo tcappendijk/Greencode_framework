acc257ee82969c47a8c48e45c7c81e9dbbcf4a54
Tom Cappendijk <tom.cappendijk@student.uva.nl>
2024-05-11 23:01:33 +0200

    added vllm to deepseek coder

--- test_code/CodeLLama.py
+++ test_code/CodeLLama.py
@@ -14,41 +14,26 @@ tokenizer = AutoTokenizer.from_pretrained(model_name, cache_dir=custom_cache_dir
 
 # Move model to device (GPU)
 model = AutoModelForCausalLM.from_pretrained(model_name, cache_dir=custom_cache_dir, token=token, torch_dtype=torch.bfloat16, device_map="balanced")
+model.to("cuda")
 
-
-# Create a pipeline
-code_generator = pipeline('text-generation', model=model, tokenizer=tokenizer, framework='pt', max_length=1000, pad_token_id=tokenizer.eos_token_id)
+# Define a function to generate code
+def generate_code(input_string, max_length=1000):
+    input_ids = tokenizer.encode(input_string, return_tensors="pt").to("cuda")
+    output = model.generate(input_ids, max_length=max_length, pad_token_id=tokenizer.eos_token_id)
+    generated_code = tokenizer.decode(output[0], skip_special_tokens=True)
+    return generated_code
 
 # Generate code for an input string
 input_string = "Write a python function to calculate the factorial of a number"
-generated_code = code_generator(input_string, max_length=100)[0]['generated_text']
+generated_code = generate_code(input_string, max_length=100)
 print(generated_code)
 
 
-# device_ids = [0, 1, 2, 3, 4, 5, 6, 7]
+# # Create a pipeline
+# code_generator = pipeline('text-generation', model=model, tokenizer=tokenizer, framework='pt', max_length=1000, pad_token_id=tokenizer.eos_token_id)
 
-# if torch.cuda.is_available():
-#     model = DDP(model)
-#     print("Model is now DDP")
-#     # model = DataParallel(model, device_ids=device_ids)
-#     # model = model.cuda()  # Move model to GPU
-#     # print("Model is now DataParallel")
+# # Generate code for an input string
+# input_string = "Write a python function to calculate the factorial of a number"
+# generated_code = code_generator(input_string, max_length=100)[0]['generated_text']
+# print(generated_code)
 
-# input_text = "Write a quick sort algorithm without test cases. Name the function quick_sort"
-
-# input_ids = tokenizer.encode(input_text, return_tensors="pt")
-
-# if torch.cuda.is_available():
-#     input_ids = input_ids.cuda()
-
-# print(input_ids)
-
-# with torch.no_grad():
-#     if torch.cuda.is_available():
-#         output = model.module.generate(input_ids.cuda(), max_new_tokens=1200, pad_token_id=tokenizer.eos_token_id)
-#     else:
-#         output = model.generate(input_ids, max_new_tokens=1200, pad_token_id=tokenizer.eos_token_id)
-
-# output_text = tokenizer.decode(output[0], skip_special_tokens=True)
-
-# print(output_text)
