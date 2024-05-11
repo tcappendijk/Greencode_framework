0583f41bde0b7f8fbe7c1f2abca286bf17497b14
Tom Cappendijk <tom.cappendijk@student.uva.nl>
2024-05-11 13:59:32 +0200

    Added meta-llama/CodeLlama-70b-hf

--- test_code/CodeLLama.py
+++ test_code/CodeLLama.py
@@ -7,7 +7,7 @@ model_name = "meta-llama/CodeLlama-7b-Instruct-hf"
 tokenizer = AutoTokenizer.from_pretrained(model_name, cache_dir=custom_cache_dir, token=token, trust_remote_code=True)
 model = AutoModelForCausalLM.from_pretrained(model_name, cache_dir=custom_cache_dir, token=token, trust_remote_code=True)
 
-input_text = "#write a quick sort algorithm. Only provide the code with no additional text"
+input_text = "#write a quick sort algorithm. Provide only the code. Name the function quick_sort"
 inputs = tokenizer(input_text, return_tensors="pt")
-outputs = model.generate(**inputs, max_length=128)
+outputs = model.generate(**inputs, max_length=128, eos_token_id=tokenizer.eos_token_id, pad_token_id=tokenizer.eos_token_id)
 print(tokenizer.decode(outputs[0], skip_special_tokens=True))
