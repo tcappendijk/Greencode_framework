37b56f7aea80c2282faa582ad45f67d57943f6f2
Tom Cappendijk <tom.cappendijk@student.uva.nl>
2024-05-11 14:26:23 +0200

    Trying to run the model on multiple gpu's

--- test_code/CodeLLama.py
+++ test_code/CodeLLama.py
@@ -23,7 +23,7 @@ if torch.cuda.is_available():
     model = model.cuda()
     model = DataParallel(model)
 
-input_text = "#write a quick sort algorithm. Provide only the code. Name the function quick_sort"
+input_text = "Write a quick sort algorithm. Provide only the code witout test cases. Name the function quick_sort"
 
 input_ids = tokenizer.encode(input_text, return_tensors="pt")
 
@@ -32,9 +32,9 @@ if torch.cuda.is_available():
 
 with torch.no_grad():
     if torch.cuda.is_available():
-        output = model.module.generate(input_ids, max_new_tokens=1200)
+        output = model.module.generate(input_ids, max_new_tokens=1200, eos_token_id=tokenizer.eos_token_id, pad_token_id=tokenizer.eos_token_id, return_full_text=False)
     else:
-        output = model.generate(input_ids, max_new_tokens=1200)
+        output = model.generate(input_ids, max_new_tokens=1200, eos_token_id=tokenizer.eos_token_id, pad_token_id=tokenizer.eos_token_id, return_full_text=False)
 
 output_text = tokenizer.decode(output[0], skip_special_tokens=True)
 
