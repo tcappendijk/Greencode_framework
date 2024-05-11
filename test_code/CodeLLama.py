6006e7593bcfa120052d3664796f841e9842ceb3
Tom Cappendijk <tom.cappendijk@student.uva.nl>
2024-05-11 13:47:17 +0200

    Added trust remote code

--- test_code/CodeLLama.py
+++ test_code/CodeLLama.py
@@ -1,35 +1,8 @@
-# import torch
-# from transformers import AutoModelForCausalLM, AutoTokenizer
-# from torch.nn.parallel import DataParallel
-
-# # Load your model
-# model_name = "../CodeLlama-70b-Python-hf"
-# tokenizer = AutoTokenizer.from_pretrained(model_name)
-# model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=torch.bfloat16).cuda()
-
-# # Check if CUDA is available
-# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-# # Move your model to the device
-# model = model.to(device)
-
-# # Wrap the model with DataParallel
-# model = DataParallel(model)
-
-# input_text = "#write a quick sort algorithm"
-# inputs = tokenizer(input_text, return_tensors="pt").to(model.device)
-# outputs = model.generate(**inputs, max_length=128)
-# print(tokenizer.decode(outputs[0], skip_special_tokens=True))
-
-# while True:
-#     pass
-
-# Load model directly
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
 custom_cache_dir = "/data/volume_2"
 
 token = "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"
-
-tokenizer = AutoTokenizer.from_pretrained("meta-llama/CodeLlama-7b-Instruct-hf", cache_dir=custom_cache_dir, token=token, trust_remote_code=True)
-model = AutoModelForCausalLM.from_pretrained("meta-llama/CodeLlama-7b-Instruct-hf", cache_dir=custom_cache_dir, token=token, trust_remote_code=True)
+model_name = "meta-llama/CodeLlama-70b-Instruct-hf"
+tokenizer = AutoTokenizer.from_pretrained(model_name, cache_dir=custom_cache_dir, token=token, trust_remote_code=True)
+model = AutoModelForCausalLM.from_pretrained(model_name, cache_dir=custom_cache_dir, token=token, trust_remote_code=True)
