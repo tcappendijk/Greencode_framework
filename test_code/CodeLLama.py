c9cf5792fe3f7cb30b7aab03ff1ce3d3bca89d84
Tom Cappendijk <tom.cappendijk@student.uva.nl>
2024-05-11 13:51:12 +0200

    Added meta-llama/CodeLlama-70b-hf

--- test_code/CodeLLama.py
+++ test_code/CodeLLama.py
@@ -3,6 +3,6 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 custom_cache_dir = "/data/volume_2"
 
 token = "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"
-model_name = "meta-llama/CodeLlama-70b-Instruct-hf"
+model_name = "meta-llama/CodeLlama-70b-hf"
 tokenizer = AutoTokenizer.from_pretrained(model_name, cache_dir=custom_cache_dir, token=token, trust_remote_code=True)
 model = AutoModelForCausalLM.from_pretrained(model_name, cache_dir=custom_cache_dir, token=token, trust_remote_code=True)
