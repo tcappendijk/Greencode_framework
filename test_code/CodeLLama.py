73e1279dd7faf26d7ec53be4944d90e7a7420bc3
Tom Cappendijk <tom.cappendijk@student.uva.nl>
2024-05-11 15:37:53 +0200

    Made a loop to download all the CodeLLama models that I do not have now

--- test_code/CodeLLama.py
+++ test_code/CodeLLama.py
@@ -2,14 +2,6 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 import torch
 from torch.nn.parallel import DataParallel
 
-# num_devices = torch.cuda.device_count()
-
-# device_ids = []
-# for i in range(num_devices):
-#     device_ids.append(i)
-#     torch.cuda.set_device(i)
-
-
 custom_cache_dir = "/data/volume_2"
 
 token = "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"
@@ -22,9 +14,8 @@ model = AutoModelForCausalLM.from_pretrained(model_name, cache_dir=custom_cache_
 device_ids = [0, 1, 2, 3, 4, 5, 6, 7]
 
 if torch.cuda.is_available():
-    # model = model.cuda()
-    # print("Model is on cuda") 
     model = DataParallel(model, device_ids=device_ids)
+    model = model.cuda()  # Move model to GPU
     print("Model is now DataParallel")
 
 input_text = "Write a quick sort algorithm without test cases. Name the function quick_sort"
@@ -38,7 +29,7 @@ print(input_ids)
 
 with torch.no_grad():
     if torch.cuda.is_available():
-        output = model.module.generate(input_ids, max_new_tokens=1200, pad_token_id=tokenizer.eos_token_id)
+        output = model.module.generate(input_ids.cuda(), max_new_tokens=1200, pad_token_id=tokenizer.eos_token_id)
     else:
         output = model.generate(input_ids, max_new_tokens=1200, pad_token_id=tokenizer.eos_token_id)
 
