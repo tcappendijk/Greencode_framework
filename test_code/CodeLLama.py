755923a9c28efcfe6c5670d678a69ee234b54145
Tom Cappendijk <tom.cappendijk@student.uva.nl>
2024-05-11 14:20:26 +0200

    Trying to run the model on multiple gpu's

--- test_code/CodeLLama.py
+++ test_code/CodeLLama.py
@@ -1,41 +1,41 @@
-# from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import AutoTokenizer, AutoModelForCausalLM
 import torch
-# from torch.nn.parallel import DistributedDataParallel
+from torch.nn.parallel import DistributedDataParallel
 
 num_devices = torch.cuda.device_count()
 
 device_ids = []
 for i in range(num_devices):
     device_ids.append(i)
+    torch.cuda.set_device(i)
 
-print(device_ids)
 
-# custom_cache_dir = "/data/volume_2"
+custom_cache_dir = "/data/volume_2"
 
-# token = "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"
-# model_name = "meta-llama/CodeLlama-7b-Instruct-hf"
+token = "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"
+model_name = "meta-llama/CodeLlama-7b-Instruct-hf"
 
-# tokenizer = AutoTokenizer.from_pretrained(model_name, cache_dir=custom_cache_dir, token=token, trust_remote_code=True)
+tokenizer = AutoTokenizer.from_pretrained(model_name, cache_dir=custom_cache_dir, token=token, trust_remote_code=True)
 
-# model = AutoModelForCausalLM.from_pretrained(model_name, cache_dir=custom_cache_dir, token=token, trust_remote_code=True, torch_dtype=torch.bfloat16)
+model = AutoModelForCausalLM.from_pretrained(model_name, cache_dir=custom_cache_dir, token=token, trust_remote_code=True, torch_dtype=torch.bfloat16)
 
-# if torch.cuda.is_available():
-#     model = model.cuda()
-#     model = DistributedDataParallel(model)
+if torch.cuda.is_available():
+    model = model.cuda()
+    model = DistributedDataParallel(model, device_ids=device_ids, output_device=device_ids[0])
 
-# input_text = "#write a quick sort algorithm. Provide only the code. Name the function quick_sort"
+input_text = "#write a quick sort algorithm. Provide only the code. Name the function quick_sort"
 
-# input_ids = tokenizer.encode(input_text, return_tensors="pt")
+input_ids = tokenizer.encode(input_text, return_tensors="pt")
 
-# if torch.cuda.is_available():
-#     input_ids = input_ids.cuda()
+if torch.cuda.is_available():
+    input_ids = input_ids.cuda()
 
-# with torch.no_grad():
-#     if torch.cuda.is_available():
-#         output = model.module.generate(input_ids, max_new_tokens=1200)
-#     else:
-#         output = model.generate(input_ids, max_new_tokens=1200)
+with torch.no_grad():
+    if torch.cuda.is_available():
+        output = model.module.generate(input_ids, max_new_tokens=1200)
+    else:
+        output = model.generate(input_ids, max_new_tokens=1200)
 
-# output_text = tokenizer.decode(output[0], skip_special_tokens=True)
+output_text = tokenizer.decode(output[0], skip_special_tokens=True)
 
-# print(output_text)
+print(output_text)
