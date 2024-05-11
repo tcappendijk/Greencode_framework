b3e108336d9bdbb6c6b8ce03f70bb5d4ca87694b
Tom Cappendijk <tom.cappendijk@student.uva.nl>
2024-05-11 23:05:26 +0200

    added vllm to deepseek coder

--- test_code/CodeLLama.py
+++ test_code/CodeLLama.py
@@ -12,9 +12,9 @@ model_name = "meta-llama/CodeLlama-70b-Instruct-hf"
 
 tokenizer = AutoTokenizer.from_pretrained(model_name, cache_dir=custom_cache_dir, token=token)
 
-model = AutoModelForCausalLM.from_pretrained(model_name, cache_dir=custom_cache_dir, token=token, torch_dtype=torch.bfloat16)
+model = AutoModelForCausalLM.from_pretrained(model_name, cache_dir=custom_cache_dir, token=token, torch_dtype=torch.bfloat16, device_map="balanced")
 
-code_generator = pipeline('text-generation', model=model, tokenizer=tokenizer, framework='pt', max_length=1000, pad_token_id=tokenizer.eos_token_id, device_map="balanced", device=0)
+code_generator = pipeline('text-generation', model=model, tokenizer=tokenizer, framework='pt', max_length=1000, pad_token_id=tokenizer.eos_token_id)
 
 # Generate code for an input string
 input_string = "Write a python function to calculate the factorial of a number"
