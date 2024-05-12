f1b920be5112d39326c4eb3a3d348fe37f6c28f1
Tom Cappendijk <tom.cappendijk@student.uva.nl>
2024-05-12 21:46:52 +0200

    Deleted the old code from both servers, including the gpu schedular

--- deepseek-coder-6.7b-instruct/server.py
+++ deepseek-coder-6.7b-instruct/server.py
@@ -1,40 +1,8 @@
 import socket
 from transformers import AutoTokenizer, AutoModelForCausalLM
 import torch
-import subprocess
 import argparse
 from transformers import pipeline
-
-
-def get_gpu_with_most_free_memory():
-    try:
-        result = subprocess.check_output(["nvidia-smi", "--query-gpu=index,memory.free,memory.total", "--format=csv,noheader,nounits"])
-
-        gpu_info = result.decode().strip().split('\n')
-        gpu_info = [info.split(', ') for info in gpu_info]
-        gpu_info = [(int(index), int(free_memory), int(total_memory)) for index, free_memory, total_memory in gpu_info]
-        gpu_info.sort(key=lambda x: x[1], reverse=True)
-        gpu_index = gpu_info[0][0]
-
-        return gpu_index
-    except Exception as e:
-        print(f"Error occurred while getting GPU information: {e}")
-        return None
-
-
-def initialize_model():
-    tokenizer = AutoTokenizer.from_pretrained("deepseek-ai/deepseek-coder-6.7b-instruct", trust_remote_code=True)
-    model = AutoModelForCausalLM.from_pretrained("deepseek-ai/deepseek-coder-6.7b-instruct", trust_remote_code=True, torch_dtype=torch.bfloat16)
-
-    # Check for the gpu with the most free memory
-    gpu_index = get_gpu_with_most_free_memory()
-    if gpu_index is None:
-        return None, None, None
-
-    gpu_device = torch.device(f"cuda:{gpu_index}")
-    model = model.to(gpu_device)
-
-    return tokenizer, model, gpu_device
 
 
 def server(host, port):
@@ -46,12 +14,6 @@ def server(host, port):
     server_socket.listen(1)
 
     print("Server started.")
-
-    # tokenizer, model, gpu_device = initialize_model()
-
-    # if tokenizer is None or model is None or gpu_device is None:
-    #     print("Error occurred while initializing the model.")
-    #     return
 
     tokenizer = AutoTokenizer.from_pretrained("deepseek-ai/deepseek-coder-6.7b-instruct", trust_remote_code=True)
     model = AutoModelForCausalLM.from_pretrained("deepseek-ai/deepseek-coder-6.7b-instruct", trust_remote_code=True, torch_dtype=torch.bfloat16, device_map="balanced")
@@ -71,14 +33,6 @@ def server(host, port):
                 client_socket.close()
                 break
 
-            messages=[
-                { 'role': 'user', 'content': prompt}
-            ]
-
-            # inputs = tokenizer.apply_chat_template(messages, add_generation_prompt=True, return_tensors="pt").to(gpu_device)
-            # outputs = model.generate(inputs, max_new_tokens=1024, num_return_sequences=1, eos_token_id=tokenizer.eos_token_id, pad_token_id=tokenizer.eos_token_id)
-
-            # output = tokenizer.decode(outputs[0][len(inputs[0]):])
             output = code_generator(prompt, max_length=1000)[0]['generated_text']
 
             client_socket.sendall(output.encode())
