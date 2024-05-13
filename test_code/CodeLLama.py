c9a37d96a800c8a2cfd400e1861f51ed097c9c27
Tom Cappendijk <tom.cappendijk@student.uva.nl>
2024-05-13 12:39:32 +0200

    Test a leetcode code prompt

--- test_code/CodeLLama.py
+++ test_code/CodeLLama.py
@@ -10,7 +10,7 @@ tokenizer = AutoTokenizer.from_pretrained(model_name, cache_dir=custom_cache_dir
 
 model = AutoModelForCausalLM.from_pretrained(model_name, cache_dir=custom_cache_dir, token=token, torch_dtype=torch.bfloat16, device_map="balanced")
 
-code_generator = pipeline('text-generation', model=model, tokenizer=tokenizer, framework='pt', pad_token_id=tokenizer.eos_token_id)
+code_generator = pipeline(model=model, tokenizer=tokenizer, framework='pt', pad_token_id=tokenizer.eos_token_id)
 
 # Generate code for an input string
 input_string = """Given the head of a linked list, return the list after sorting it in ascending order.
