f558415a7d53a2c85a6775c4592748875333f1ba
Tom Cappendijk <tom.cappendijk@student.uva.nl>
2024-05-13 12:12:56 +0200

    Test a leetcode code prompt

--- test_code/CodeLLama.py
+++ test_code/CodeLLama.py
@@ -4,7 +4,7 @@ from transformers import pipeline
 
 custom_cache_dir = "/data/volume_2"
 token = "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"
-model_name = "deepseek-ai/deepseek-coder-33b-instruct"
+model_name = "meta-llama/CodeLlama-70b-Instruct-hf"
 
 tokenizer = AutoTokenizer.from_pretrained(model_name, cache_dir=custom_cache_dir, token=token)
 
@@ -13,7 +13,29 @@ model = AutoModelForCausalLM.from_pretrained(model_name, cache_dir=custom_cache_
 code_generator = pipeline('text-generation', model=model, tokenizer=tokenizer, framework='pt', pad_token_id=tokenizer.eos_token_id)
 
 # Generate code for an input string
-input_string = "Write a python function to calculate the factorial of a number, only provide the code with no additional text."
+input_string = """
+Given a list of numbers arranged in a specific order, return the list with its numbers sorted from smallest to largest.
+
+Example 1:
+Input: [4,2,1,3]
+Output: [1,2,3,4]
+
+Example 2:
+Input: [-1,5,3,4,0]
+Output: [-1,0,3,4,5]
+
+Example 3:
+Input: []
+Output: []
+
+Constraints:
+- The list can have at most 50,000 numbers.
+- Each number in the list is between -100,000 and 100,000.
+
+Follow up:
+Can you sort the list efficiently in terms of both time and memory usage?
+"""
+
 generated_code = code_generator(input_string, max_length=1000)[0]['generated_text']
 print(generated_code)
 
