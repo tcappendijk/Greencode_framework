b87e911dff29eeacffaa9d1f246b5793caabc824
Tom Cappendijk <tom.cappendijk@student.uva.nl>
2024-05-16 15:39:27 +0200

    added cache directory

--- test_code/chatgpt.py
+++ test_code/chatgpt.py
@@ -1,4 +1,12 @@
 from transformers import pipeline, set_seed
-generator = pipeline('text-generation', model='gpt2-large', device_map="balanced")
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import torch
+
+custom_cache_dir = "/data/volume_2"
+tokenizer = AutoTokenizer.from_pretrained("openai-community/gpt2-large", custom_cache_dir=custom_cache_dir)
+model = AutoModelForCausalLM.from_pretrained("openai-community/gpt2-large", custom_cache_dir=custom_cache_dir)
+
+
+generator = pipeline('text-generation', model=model, tokenizer=tokenizer)
 set_seed(42)
 generator("Hello, I'm a language model,", max_length=30, num_return_sequences=5)
