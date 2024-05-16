23adaef342f68499b60f44de943987bbb9aeb5a6
Tom Cappendijk <tom.cappendijk@student.uva.nl>
2024-05-16 15:56:09 +0200

    Removed the size of the parameters of the model and made the mapping onto one gpu

--- test_code/chatgpt.py
+++ test_code/chatgpt.py
@@ -10,7 +10,7 @@ tokenizer = AutoTokenizer.from_pretrained(model_name, cache_dir=custom_cache_dir
 
 model = AutoModelForCausalLM.from_pretrained(model_name, cache_dir=custom_cache_dir, token=token, device_map="sequential")
 
-code_generator = pipeline('text-generation', model=model, tokenizer=tokenizer, framework='pt', pad_token_id=tokenizer.eos_token_id)
+code_generator = pipeline('text-generation', model=model, tokenizer=tokenizer)
 
 # Generate code for an input string
 input_string = "Write a quicksort algorithm in python."
