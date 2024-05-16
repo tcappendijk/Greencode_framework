175a1fc038be9e0b245095da3058f684176b9d6b
Tom Cappendijk <tom.cappendijk@student.uva.nl>
2024-05-16 16:34:38 +0200

    Try deepseek coder with sequantial device map

--- test_code/Deepseek-coder.py
+++ test_code/Deepseek-coder.py
@@ -10,10 +10,10 @@ tokenizer = AutoTokenizer.from_pretrained(model_name, cache_dir=custom_cache_dir
 
 model = AutoModelForCausalLM.from_pretrained(model_name, cache_dir=custom_cache_dir, token=token, torch_dtype=torch.bfloat16, device_map="sequential")
 
-code_generator = pipeline('text-generation', model=model, tokenizer=tokenizer, framework='pt', pad_token_id=tokenizer.eos_token_id)
+code_generator = pipeline('text-generation', model=model, tokenizer=tokenizer)
 
-input_string = "Write a quicksort algorithm in python."
-generated_code = code_generator(input_string, max_length=1000)
+input_string = "Write a quicksort algorithm"
+generated_code = code_generator(input_string, return_text=True, return_full_text = True)
 print(generated_code)
 print(generated_code[0]['generated_text'])
 
