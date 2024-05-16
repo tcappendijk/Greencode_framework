52808d56d8b9c56e088dedbc2c55668c04b81ead
Tom Cappendijk <tom.cappendijk@student.uva.nl>
2024-05-16 15:47:30 +0200

    added cache directory

--- test_code/chatgpt.py
+++ test_code/chatgpt.py
@@ -13,49 +13,8 @@ model = AutoModelForCausalLM.from_pretrained(model_name, cache_dir=custom_cache_
 code_generator = pipeline('text-generation', model=model, tokenizer=tokenizer, framework='pt', pad_token_id=tokenizer.eos_token_id)
 
 # Generate code for an input string
-input_string = """Given the head of a linked list, return the list after sorting it in ascending order.
-Example 1:
+input_string = """Write a quicksort algorithm in python"""
 
-Input: head = [4,2,1,3]
-Output: [1,2,3,4]
-
-Example 2:
-
-Input: head = [-1,5,3,4,0]
-Output: [-1,0,3,4,5]
-
-Example 3:
-
-Input: head = []
-Output: []
-
-Constraints:
-
-    The number of nodes in the list is in the range [0, 5 * 104].
-    -105 <= Node.val <= 105
-
-# Definition for singly-linked list.
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-class Solution:
-    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-"""
-
-# from transformers import AutoTokenizer, AutoModelForCausalLM
-# import torch
-# from transformers import pipeline
-
-# custom_cache_dir = "/data/volume_2"
-# token = "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"
-# model_name = "openai-community/gpt2-large"
-
-# tokenizer = AutoTokenizer.from_pretrained(model_name, cache_dir=custom_cache_dir, token=token)
-
-# model = AutoModelForCausalLM.from_pretrained(model_name, cache_dir=custom_cache_dir, token=token, torch_dtype=torch.bfloat16, device_map="balanced")
-
-# generator = pipeline('text-generation', model=model, tokenizer=tokenizer, framework='pt', pad_token_id=tokenizer.eos_token_id)
 generated_code = code_generator(input_string, max_length=1000)
 print(generated_code)
 print(generated_code[0]['generated_text'])
