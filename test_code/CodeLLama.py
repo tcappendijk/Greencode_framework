ed5d72e21a3f60d1ebef7dd5098f5bdcb73f7361
Tom Cappendijk <tom.cappendijk@student.uva.nl>
2024-05-16 16:41:32 +0200

    Changed the Codellama impelementation

--- test_code/CodeLLama.py
+++ test_code/CodeLLama.py
@@ -1,4 +1,5 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import AutoTokenizer, LlamaForCausalLM
 import torch
 from transformers import pipeline
 
@@ -8,9 +9,9 @@ model_name = "meta-llama/CodeLlama-70b-Instruct-hf"
 
 tokenizer = AutoTokenizer.from_pretrained(model_name, cache_dir=custom_cache_dir, token=token)
 
-model = AutoModelForCausalLM.from_pretrained(model_name, cache_dir=custom_cache_dir, token=token, torch_dtype=torch.bfloat16, device_map="balanced")
+model = LlamaForCausalLM.from_pretrained(model_name, cache_dir=custom_cache_dir, token=token, torch_dtype=torch.bfloat16, device_map="balanced")
 
-code_generator = pipeline(model=model, tokenizer=tokenizer, framework='pt', pad_token_id=tokenizer.eos_token_id)
+code_generator = pipeline('text-generation', model=model, tokenizer=tokenizer, framework='pt', pad_token_id=tokenizer.eos_token_id)
 
 # Generate code for an input string
 input_string = """Given the head of a linked list, return the list after sorting it in ascending order.
