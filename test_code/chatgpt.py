11b5502dfdc05afd73c43c3f86ef82612cd180ce
Tom Cappendijk <tom.cappendijk@student.uva.nl>
2024-05-16 15:48:46 +0200

    Try the xl model

--- test_code/chatgpt.py
+++ test_code/chatgpt.py
@@ -4,11 +4,11 @@ from transformers import pipeline
 
 custom_cache_dir = "/data/volume_2"
 token = "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"
-model_name = "openai-community/gpt2-large"
+model_name = "openai-community/gpt2-xl"
 
 tokenizer = AutoTokenizer.from_pretrained(model_name, cache_dir=custom_cache_dir, token=token)
 
-model = AutoModelForCausalLM.from_pretrained(model_name, cache_dir=custom_cache_dir, token=token, torch_dtype=torch.bfloat16, device_map="balanced")
+model = AutoModelForCausalLM.from_pretrained(model_name, cache_dir=custom_cache_dir, token=token, torch_dtype=torch.bfloat16)
 
 code_generator = pipeline('text-generation', model=model, tokenizer=tokenizer, framework='pt', pad_token_id=tokenizer.eos_token_id)
 
