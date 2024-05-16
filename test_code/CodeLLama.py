0cf1025ab7db7e7f4f1102e6013f2a8b5d35602f
Tom Cappendijk <tom.cappendijk@student.uva.nl>
2024-05-16 16:50:00 +0200

    Changed the Codellama impelementation

--- test_code/CodeLLama.py
+++ test_code/CodeLLama.py
@@ -6,7 +6,7 @@ custom_cache_dir = "/data/volume_2"
 token = "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"
 model_name = "meta-llama/CodeLlama-70b-Instruct-hf"
 
-tokenizer = AutoTokenizer.from_pretrained(model_name, cache_dir=custom_cache_dir, token=token)
+tokenizer = AutoTokenizer.from_pretrained(model_name, cache_dir=custom_cache_dir, token=token, Truncation=True)
 
 model = LlamaForCausalLM.from_pretrained(model_name, cache_dir=custom_cache_dir, token=token, torch_dtype=torch.bfloat16, device_map="balanced")
 
@@ -45,7 +45,7 @@ class Solution:
 # input_string = "Given the head of a linked list, return the list after sorting it in ascending order.    Example 1:  Input: head = [4,2,1,3] Output: [1,2,3,4]  Example 2:  Input: head = [-1,5,3,4,0] Output: [-1,0,3,4,5]  Example 3:  Input: head = [] Output: []    Constraints:  The number of nodes in the list is in the range [0, 5 * 104]. -105 <= Node.val <= 105    Follow up: Can you sort the linked list in O(n logn) time and O(1) memory (i.e. constant space)?  # Definition for singly-linked list. # class ListNode: #     def __init__(self, val=0, next=None): #         self.val = val #         self.next = next class Solution: def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]: "
 
 
-generated_code = code_generator(input_string, max_length=1000, Truncation=True, return_full_text = False)
+generated_code = code_generator(input_string, return_full_text = False)
 print(generated_code)
 print(generated_code[0]['generated_text'])
 
