61247a3985649e86af42bc49b30d68f099553549
Tom Cappendijk <tom.cappendijk@student.uva.nl>
2024-05-16 16:53:05 +0200

    Changed the Codellama impelementation

--- test_code/CodeLLama.py
+++ test_code/CodeLLama.py
@@ -45,7 +45,7 @@ class Solution:
 # input_string = "Given the head of a linked list, return the list after sorting it in ascending order.    Example 1:  Input: head = [4,2,1,3] Output: [1,2,3,4]  Example 2:  Input: head = [-1,5,3,4,0] Output: [-1,0,3,4,5]  Example 3:  Input: head = [] Output: []    Constraints:  The number of nodes in the list is in the range [0, 5 * 104]. -105 <= Node.val <= 105    Follow up: Can you sort the linked list in O(n logn) time and O(1) memory (i.e. constant space)?  # Definition for singly-linked list. # class ListNode: #     def __init__(self, val=0, next=None): #         self.val = val #         self.next = next class Solution: def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]: "
 
 
-generated_code = code_generator(input_string, return_full_text = False, max_new_tokens=1000, do_sample=True, temperature=0.7, top_p=0.9, top_k=50, num_return_sequences=1, pad_token_id=tokenizer.eos_token_id, return_tensors=False, framework='pt')
+generated_code = code_generator(input_string, max_new_tokens=1000, do_sample=True, temperature=0.7, top_p=0.9, top_k=50, num_return_sequences=1, pad_token_id=tokenizer.eos_token_id)
 print(generated_code)
 print(generated_code[0]['generated_text'])
 
