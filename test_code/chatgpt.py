298a37bdc0da048dc42a5e4194bfb19abe5405e1
Tom Cappendijk <tom.cappendijk@student.uva.nl>
2024-05-16 15:45:32 +0200

    added cache directory

--- test_code/chatgpt.py
+++ test_code/chatgpt.py
@@ -1,16 +1,47 @@
-from transformers import pipeline, set_seed
 from transformers import AutoTokenizer, AutoModelForCausalLM
 import torch
+from transformers import pipeline
 
 custom_cache_dir = "/data/volume_2"
-tokenizer = AutoTokenizer.from_pretrained("openai-community/gpt2-large", cache_dir=custom_cache_dir)
-model = AutoModelForCausalLM.from_pretrained("openai-community/gpt2-large", cache_dir=custom_cache_dir)
+token = "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"
+model_name = "openai-community/gpt2-large"
 
+tokenizer = AutoTokenizer.from_pretrained(model_name, cache_dir=custom_cache_dir, token=token)
 
-generator = pipeline('text-generation', model=model, tokenizer=tokenizer)
-set_seed(42)
-generator("Hello, I'm a language model,", max_length=30, num_return_sequences=5)
+model = AutoModelForCausalLM.from_pretrained(model_name, cache_dir=custom_cache_dir, token=token, torch_dtype=torch.bfloat16, device_map="balanced")
 
+code_generator = pipeline('text-generation', model=model, tokenizer=tokenizer, framework='pt', pad_token_id=tokenizer.eos_token_id)
+
+# Generate code for an input string
+input_string = """Given the head of a linked list, return the list after sorting it in ascending order.
+Example 1:
+
+Input: head = [4,2,1,3]
+Output: [1,2,3,4]
+
+Example 2:
+
+Input: head = [-1,5,3,4,0]
+Output: [-1,0,3,4,5]
+
+Example 3:
+
+Input: head = []
+Output: []
+
+Constraints:
+
+    The number of nodes in the list is in the range [0, 5 * 104].
+    -105 <= Node.val <= 105
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+"""
 
 # from transformers import AutoTokenizer, AutoModelForCausalLM
 # import torch
