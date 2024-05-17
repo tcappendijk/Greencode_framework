4daa0be80e52e47e1693a8a8fac197e8c704f819
Tom Cappendijk <tom.cappendijk@student.uva.nl>
2024-05-17 17:29:01 +0200

    Made the prompt a command line argument

--- CodeLlama-70b-Instruct-hf/handle_prompt.py
+++ CodeLlama-70b-Instruct-hf/handle_prompt.py
@@ -8,6 +8,7 @@ from llama import Llama
 def main(
     ckpt_dir: str,
     tokenizer_path: str,
+    prompt: str,
     temperature: float = 0.2,
     top_p: float = 0.95,
     max_seq_len: int = 512,
@@ -21,35 +22,7 @@ def main(
         max_batch_size=max_batch_size,
     )
 
-    prompts = ["""Given the head of a linked list, return the list after sorting it in ascending order.
-Example 1:
-
-Input: head = [4,2,1,3]
-Output: [1,2,3,4]
-
-Example 2:
-
-Input: head = [-1,5,3,4,0]
-Output: [-1,0,3,4,5]
-
-Example 3:
-
-Input: head = []
-Output: []
-
-Constraints:
-
-    The number of nodes in the list is in the range [0, 5 * 104].
-    -105 <= Node.val <= 105
-
-# Definition for singly-linked list.
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-class Solution:
-    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-"""]
+    prompts = [prompt]
     results = generator.text_completion(
         prompts,
         max_gen_len=max_gen_len,
