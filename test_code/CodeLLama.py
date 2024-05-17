9ff2292e6b174371c7d631ffc3ebaa7346f70299
Tom Cappendijk <tom.cappendijk@student.uva.nl>
2024-05-17 21:40:51 +0200

    Added the option to put a variable input into the codellama file

--- test_code/CodeLLama.py
+++ test_code/CodeLLama.py
@@ -9,6 +9,8 @@ def generate_code(prompt, model_name, max_length):
 
     tokenizer = AutoTokenizer.from_pretrained(model_name, cache_dir=custom_cache_dir, token=token, truncation=True)
 
+    tokenizer.pad_token = tokenizer.eos_token
+
     model = LlamaForCausalLM.from_pretrained(model_name, cache_dir=custom_cache_dir, token=token, torch_dtype=torch.bfloat16, device_map="sequential")
 
     code_generator = pipeline('text-generation', model=model, tokenizer=tokenizer, framework='pt', pad_token_id=tokenizer.eos_token_id)
