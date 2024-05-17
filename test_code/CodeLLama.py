7e007df7fd283a5cc504a63f5c92e4990594d2e2
Tom Cappendijk <tom.cappendijk@student.uva.nl>
2024-05-17 17:51:22 +0200

    Added the option to put a variable input into the codellama file

--- test_code/CodeLLama.py
+++ test_code/CodeLLama.py
@@ -3,7 +3,7 @@ import torch
 from transformers import pipeline
 import argparse
 
-def generate_code(input_string):
+def generate_code(prompt):
     custom_cache_dir = "/data/volume_2"
     token = "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"
     model_name = "meta-llama/CodeLlama-70b-Instruct-hf"
@@ -14,11 +14,11 @@ def generate_code(input_string):
 
     code_generator = pipeline('text-generation', model=model, tokenizer=tokenizer, framework='pt', pad_token_id=tokenizer.eos_token_id)
 
-    generated_code = code_generator(input_string, do_sample=True, temperature=0.2, top_p=0.95, truncation=True, padding=False, max_length=512, num_return_sequences=1, pad_token_id=tokenizer.eos_token_id)
+    generated_code = code_generator(prompt, do_sample=True, temperature=0.7, top_p=0.95, truncation=True, padding=False, max_length=512, num_return_sequences=1, pad_token_id=tokenizer.eos_token_id)
     print(generated_code[0]['generated_text'])
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Code Generation")
-    parser.add_argument("input_string", type=str, help="Input string")
+    parser.add_argument("--prompt", type=str, help="Input prompt")
     args = parser.parse_args()
-    generate_code(args.input_string)
+    generate_code(args.prompt)
