52d4bebea2b758b94baf3e433de13fb53c3e0ff7
Tom Cappendijk <tom.cappendijk@student.uva.nl>
2024-05-19 14:55:47 +0200

    Test the deepseekcoder base model

--- test_code/Deepseek-coder.py
+++ test_code/Deepseek-coder.py
@@ -33,7 +33,7 @@ class Solution:
 """
 
 inputs = tokenizer(prompt, return_tensors="pt").to(model.device)
-outputs = model.generate(**inputs, max_length=128)
+outputs = model.generate(**inputs, max_new_tokens=512)
 print(tokenizer.decode(outputs[0], skip_special_tokens=True))
 # messages=[
 #     { 'role': 'user', 'content': prompt}
