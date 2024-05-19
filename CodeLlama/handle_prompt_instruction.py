facfc16d65ecd21ada6fcc11453f2229280e83f8
Tom Cappendijk <tom.cappendijk@student.uva.nl>
2024-05-19 21:04:46 +0200

    Deleted the old codellama specific folder and added a unified folder in order to work with every codellama instant

--- CodeLlama/handle_prompt_instruction.py
+++ CodeLlama/handle_prompt_instruction.py
@@ -11,7 +11,7 @@ def main(
     prompt: str,
     temperature: float = 0.2,
     top_p: float = 0.95,
-    max_seq_len: int = 1024,
+    max_seq_len: int = 512,
     max_batch_size: int = 8,
     max_gen_len: Optional[int] = None,
 ):
@@ -26,41 +26,7 @@ def main(
         [
             {
                 "role": "user",
-                "content": """Given the head of a linked list, return the list after sorting it in ascending order.
-
-
-
-Example 1:
-
-Input: head = [4,2,1,3]
-Output: [1,2,3,4]
-
-Example 2:
-
-Input: head = [-1,5,3,4,0]
-Output: [-1,0,3,4,5]
-
-Example 3:
-
-Input: head = []
-Output: []
-
-
-
-Constraints:
-
-    The number of nodes in the list is in the range [0, 5 * 104].
-    -105 <= Node.val <= 105
-
-# Definition for singly-linked list.
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-class Solution:
-    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-
-""",
+                "content": prompt,
             }
         ],
     ]
