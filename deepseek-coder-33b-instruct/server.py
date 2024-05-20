e2f55a221c71e785219316ad5ae373a8e98f79c2
Tom Cappendijk <tom.cappendijk@student.uva.nl>
2024-05-20 10:33:04 +0200

    Deleted the old codellama specific folder and added a unified folder in order to work with every codellama instant

---
+++ deepseek-coder-33b-instruct/server.py
@@ -0,0 +1,61 @@
+import socket
+from transformers import pipeline
+from transformers import AutoTokenizer, LlamaForCausalLM
+import torch
+import argparse
+
+
+def server(host, port):
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    server_address = (host, port)
+    server_socket.bind(server_address)
+
+    server_socket.listen(1)
+
+    print("Server started.")
+
+    custom_cache_dir = "/data/volume_2"
+    token = "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"
+    model_name = "deepseek-ai/deepseek-coder-33b-instruct"
+    tokenizer = AutoTokenizer.from_pretrained(model_name, cache_dir=custom_cache_dir, token=token)
+    model = LlamaForCausalLM.from_pretrained(model_name, cache_dir=custom_cache_dir, token=token, torch_dtype=torch.bfloat16, device_map="balanced")
+    code_generator = pipeline('text-generation', model=model, tokenizer=tokenizer, framework='pt', pad_token_id=tokenizer.eos_token_id)
+
+
+    print("Server is listening...")
+
+    max_length = 2048
+
+    while True:
+        client_socket, client_address = server_socket.accept()
+
+        try:
+            prompt = client_socket.recv(max_length)
+            prompt = prompt.decode()
+
+            if prompt == "exit":
+                client_socket.close()
+                break
+
+            output = code_generator(prompt, max_length=1000)
+
+            client_socket.sendall(output.encode())
+        finally:
+            client_socket.close()
+
+    server_socket.close()
+
+def main():
+    parser = argparse.ArgumentParser(description="Server for DeepSeekCoder 6.7b Instruct")
+    parser.add_argument("--host", type=str, help="Host to bind the server to", default="localhost")
+    parser.add_argument("--port", type=int, help="Port to bind the server to", default=12345)
+
+    args = parser.parse_args()
+    host = args.host
+    port = args.port
+
+    server(host=host, port=port)
+
+if __name__ == "__main__":
+    main()
